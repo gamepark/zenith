d3d87148cf18ff86b3b419e71be943399e4467db
Patrick Beaume <p.beaume@gmail.com>
2025-06-07 14:58:24 +0200

    specialize conditional header

--- app/src/headers/ConditionalHeader.tsx
+++ app/src/headers/ConditionalHeader.tsx
@@ -1,8 +1,9 @@
 /** @jsxImportSource @emotion/react */
 import { PlayMoveButton, useLegalMove, useLegalMoves, useRules } from '@gamepark/react-game'
-import { CustomMove, isCustomMoveType, MaterialMove, MaterialRules } from '@gamepark/rules-api'
+import { CustomMove, isCustomMoveType, isMoveItemType, MaterialMove, MaterialRules } from '@gamepark/rules-api'
 import { ConditionalEffect, ConditionType, Effect } from '@gamepark/zenith/material/effect/Effect'
 import { EffectType } from '@gamepark/zenith/material/effect/EffectType'
+import { MaterialType } from '@gamepark/zenith/material/MaterialType'
 import { CustomMoveType } from '@gamepark/zenith/rules/CustomMoveType'
 import { Memory } from '@gamepark/zenith/rules/Memory'
 
@@ -17,6 +18,17 @@ export const ConditionalHeader = () => {
       effect.condition.effect.type === EffectType.SpendCredit &&
       isCustomMoveType(CustomMoveType.DoCondition)(move)
   )
+
+  const giveLeaderBadge = useLegalMove((move: MaterialMove) => isMoveItemType(MaterialType.LeaderBadgeToken)(move))
+  if (giveLeaderBadge) {
+    return (
+      <>
+        <PlayMoveButton move={pass}>Passez</PlayMoveButton>
+        <PlayMoveButton move={giveLeaderBadge}>Donner le badge</PlayMoveButton>
+      </>
+    )
+  }
+
   if (spendCredits.length) {
     return (
       <>
