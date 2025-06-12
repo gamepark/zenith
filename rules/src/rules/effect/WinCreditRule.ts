21f15d786cd53f018605285bd1aefa2a9ad80696
Patrick Beaume <p.beaume@gmail.com>
2025-06-12 08:53:08 +0200

    Handle other kind of win credit effect

--- rules/src/rules/effect/WinCreditRule.ts
+++ rules/src/rules/effect/WinCreditRule.ts
@@ -1,7 +1,10 @@
 import { MaterialMove } from '@gamepark/rules-api'
+import { Agent } from '../../material/Agent'
 import { WinCreditEffect } from '../../material/effect/Effect'
 import { LocationType } from '../../material/LocationType'
+import { MaterialType } from '../../material/MaterialType'
 import { EffectRule } from './index'
+import uniq from 'lodash/uniq'
 
 export class WinCreditRule extends EffectRule<WinCreditEffect> {
   onRuleStart() {
@@ -16,8 +19,41 @@ export class WinCreditRule extends EffectRule<WinCreditEffect> {
 
   get wonCredit() {
     if (this.effect.quantity) return this.effect.quantity
-    // TODO: other effects
+    if (this.effect.perLevel1Technology) return this.effect.perLevel1Technology[this.level1Technology - 1]
+    if (this.effect.factorPerDifferentInfluence) return this.distinctInfluence * this.effect.factorPerDifferentInfluence
+    if (this.effect.factorPerDifferentOpponentInfluence) return this.opponentDistinctInfluence * this.effect.factorPerDifferentOpponentInfluence
     return 0
+  }
+
+  isPossible(): boolean {
+    if (this.effect.perLevel1Technology) return this.level1Technology > 0
+    if (this.effect.factorPerDifferentInfluence) return this.distinctInfluence > 0
+    if (this.effect.factorPerDifferentOpponentInfluence) return this.opponentDistinctInfluence > 0
+    return true
+  }
+
+  get distinctInfluence() {
+    const items = this.material(MaterialType.AgentCard)
+      .location(LocationType.Influence)
+      .player(this.playerHelper.team)
+      .getItems<Agent>()
+      .map((a) => a.id)
+    return uniq(items).length
+  }
+
+  get opponentDistinctInfluence() {
+    const items = this.material(MaterialType.AgentCard)
+      .location(LocationType.Influence)
+      .player(this.opponentTeam)
+      .getItems<Agent>()
+      .map((a) => a.id)
+    return uniq(items).length
+  }
+
+  get level1Technology() {
+    return this.material(MaterialType.TechnologyBoard)
+      .player(this.playerHelper.team)
+      .location((l) => l.type === LocationType.TechnologyBoardTokenSpace && l.x === 1).length
   }
 
   setExtraData(_extraData: Record<string, unknown>) {
