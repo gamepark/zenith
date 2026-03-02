f5265cc1702a35ddb25f1139fa21ddecf1ccd482
Patrick Beaume <p.beaume@gmail.com>
2026-03-02 08:23:46 +0100

    Add missing headers for all automatic effect rules (mobilize, steal credit, give/spend credit/zenithium, give leader badge)

---
+++ app/src/headers/AutoEffectHeader.tsx
@@ -0,0 +1,44 @@
+/** @jsxImportSource @emotion/react */
+import { usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
+import { MaterialRules } from '@gamepark/rules-api'
+import { ExpandedEffect } from '@gamepark/zenith/material/effect/Effect'
+import { PlayerId } from '@gamepark/zenith/PlayerId'
+import { Memory } from '@gamepark/zenith/rules/Memory'
+import { getTeamColor } from '@gamepark/zenith/TeamColor'
+import { FC } from 'react'
+import { Trans, useTranslation } from 'react-i18next'
+import { HeaderTransComponents } from '../i18n/trans.components'
+import { EffectSource } from './EffectSource'
+
+type AutoEffectHeaderProps = {
+  i18nKey: string
+  defaults: string
+  defaultsMe: string
+}
+
+export const AutoEffectHeader: FC<AutoEffectHeaderProps> = ({ i18nKey, defaults, defaultsMe }) => {
+  const rules = useRules<MaterialRules>()!
+  const me = usePlayerId<PlayerId>()
+  const activePlayer = rules.getActivePlayer()
+  const itsMe = me === activePlayer
+  const name = usePlayerName(activePlayer)
+  const { t } = useTranslation()
+  const currentEffect = rules.remind<ExpandedEffect | undefined>(Memory.CurrentEffect as number)
+  const effects = rules.remind<ExpandedEffect[] | undefined>(Memory.Effects as number)
+  const effectSource = currentEffect?.effectSource ?? effects?.[0]?.effectSource
+  const source = effectSource ? <EffectSource effectSource={effectSource} /> : <></>
+  const components = { ...HeaderTransComponents, source }
+
+  if (itsMe) {
+    return <Trans i18nKey={`${i18nKey}.me`} defaults={defaultsMe} components={components} />
+  }
+
+  return (
+    <Trans
+      i18nKey={i18nKey}
+      defaults={defaults}
+      values={{ player: name, team: activePlayer ? t(`team.${getTeamColor(activePlayer)}`) : '' }}
+      components={components}
+    />
+  )
+}
--- app/src/headers/Headers.tsx
+++ app/src/headers/Headers.tsx
@@ -1,6 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import { RuleId } from '@gamepark/zenith/rules/RuleId'
 import { ComponentType } from 'react'
+import { AutoEffectHeader } from './AutoEffectHeader'
 import { ChoiceHeader } from './ChoiceHeader'
 import { ConditionalHeader } from './ConditionalHeader'
 import { DevelopTechnologyHeader } from './DevelopTechnologyHeader'
@@ -22,23 +23,38 @@ import { WinCreditHeader } from './WinCreditHeader'
 import { WinInfluenceHeader } from './WinInfluenceHeader'
 import { WinZenithiumHeader } from './WinZenithiumHeader'
 
+const MobilizeHeader = () => <AutoEffectHeader i18nKey="header.mobilize" defaultsMe="<source /> : You mobilize agents" defaults="<source /> : Team {team} mobilizes agents" />
+const StealCreditHeader = () => <AutoEffectHeader i18nKey="header.steal-credit" defaultsMe="<source /> : You steal credits from your opponents" defaults="<source /> : Team {team} steals credits" />
+const GiveCreditHeader = () => <AutoEffectHeader i18nKey="header.give-credit" defaultsMe="<source /> : You give credits to your opponents" defaults="<source /> : Team {team} gives credits" />
+const SpendCreditHeader = () => <AutoEffectHeader i18nKey="header.spend-credit" defaultsMe="<source /> : You spend credits" defaults="<source /> : Team {team} spends credits" />
+const GiveZenithiumHeader = () => <AutoEffectHeader i18nKey="header.give-zenithium" defaultsMe="<source /> : You give zenithium to your opponents" defaults="<source /> : Team {team} gives zenithium" />
+const SpendZenithiumHeader = () => <AutoEffectHeader i18nKey="header.spend-zenithium" defaultsMe="<source /> : You spend zenithium" defaults="<source /> : Team {team} spends zenithium" />
+const GiveLeaderBadgeHeader = () => <AutoEffectHeader i18nKey="header.give-leader-badge" defaultsMe="<source /> : You give the Leader Badge to your opponents" defaults="<source /> : Team {team} gives the Leader Badge" />
+
 export const Headers: Partial<Record<RuleId, ComponentType>> = {
   [RuleId.Muligan]: MuliganHeader,
   [RuleId.PlayCard]: PlayCardHeader,
   [RuleId.DiscardAction]: DiscardActionHeader,
   [RuleId.Transfer]: TransfertHeader,
+  [RuleId.GiveCredit]: GiveCreditHeader,
+  [RuleId.SpendCredit]: SpendCreditHeader,
   [RuleId.GiveInfluence]: GiveInfluenceHeader,
   [RuleId.ResetInfluence]: ResetInfluenceHeader,
   [RuleId.Conditional]: ConditionalHeader,
   [RuleId.DevelopTechnology]: DevelopTechnologyHeader,
+  [RuleId.GiveZenithium]: GiveZenithiumHeader,
+  [RuleId.SpendZenithium]: SpendZenithiumHeader,
   [RuleId.Choice]: ChoiceHeader,
   [RuleId.Exile]: ExileHeader,
   [RuleId.TakeBonus]: TakeBonusTokenHeader,
   [RuleId.TakeLeaderBadge]: TakeLeaderBadgeHeader,
+  [RuleId.GiveLeaderBadge]: GiveLeaderBadgeHeader,
   [RuleId.WinCredit]: WinCreditHeader,
   [RuleId.WinZenithium]: WinZenithiumHeader,
   [RuleId.WinInfluence]: WinInfluenceHeader,
   [RuleId.Discard]: DiscardHeader,
+  [RuleId.Mobilize]: MobilizeHeader,
+  [RuleId.StealCredit]: StealCreditHeader,
   [RuleId.TakeTechnologyBonusToken]: TakeTechnologyBonusTokenHeader,
   [RuleId.Refill]: RefillHeader,
   [RuleId.PickOrder]: PickOrderHeader,
