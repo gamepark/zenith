8b604e27047d64ecf191eeabef77156274632982
Romain Fromi <romain.fromi@gmail.com>
2026-03-26 17:07:34 +0100

    Add missing version import in vite config

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { defineConfig, loadEnv } from 'vite'
 import { imagetools } from 'vite-imagetools'
+import { version } from './package.json'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
